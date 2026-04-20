af8afde590f8ef9aa4305b31ef7d849107b9fb9b
David Chan Keng Hoe <chan050516@gmail.com>
2026-04-21 03:11:02 +0800

    add run.py

---
+++ backend/run.py
@@ -0,0 +1,8 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
